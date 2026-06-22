8fdae79c6656360eba51dec6bb7c36c353d4334e
leeeeeseojin <kkyuuu00@gmail.com>
2026-06-23 05:59:26 +0900

    design : 플로어맵 CSS (#24) (#84)
    Co-authored-by: Hyunji <eine113589@gmail.com>

--- src/pages/main-page/components/filter-bar/FilterBar.jsx
+++ src/pages/main-page/components/filter-bar/FilterBar.jsx
@@ -1,23 +1,21 @@
 import './FilterBar.css'
-import CalendarIcon from '../../../../assets/filter-bar/calendar-icon.svg'
-import DropdownIcon from '../../../../assets/filter-bar/dropdown-icon.svg'
 
 export default function FilterBar() {
   return (
     <div className='filter-bar'>
       <div className='filter-bar__toggle'>
-        <img src={CalendarIcon} alt='캘린더' className='filter-bar__toggle-calendar' />
+        <div className='filter-bar__toggle-calendar' />
         <span className='filter-bar__toggle-text'>06/16/26</span>
       </div>
 
       <div className='filter-bar__toggle'>
         <span className='filter-bar__toggle-text'>시간</span>
-        <img src={DropdownIcon} alt='' className='filter-bar__toggle-icon' />
+        <span className='filter-bar__toggle-icon' />
       </div>
 
       <div className='filter-bar__toggle'>
         <span className='filter-bar__toggle-text'>1층</span>
-        <img src={DropdownIcon} alt='' className='filter-bar__toggle-icon' />
+        <span className='filter-bar__toggle-icon' />
       </div>
     </div>
   )
