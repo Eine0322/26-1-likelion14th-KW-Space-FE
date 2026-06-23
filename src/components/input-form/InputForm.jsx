a9543cb3b8bcdfaf3cbe729cb25bf519d48842c7
dddan0303 <dddan0303@gmail.com>
2026-06-24 06:24:59 +0900

    feat : inputForm 컴포넌트 props 연결 (#108) (#111)

--- src/components/input-form/InputForm.jsx
+++ src/components/input-form/InputForm.jsx
@@ -1,11 +1,40 @@
-function InputForm() {
+﻿import './InputForm.css'
+
+function InputForm({
+  type = 'text',
+  placeholder = '',
+  value,
+  name,
+  id,
+  leftIcon,
+  rightIcon,
+  onChange,
+  disabled = false,
+}) {
   return (
     <div className="input-form">
-      <div className="input-form__left-icon" aria-hidden="true" />
+      {leftIcon && (
+        <div className="input-form__left-icon" aria-hidden="true">
+          {leftIcon}
+        </div>
+      )}
 
-      <input className="input-form__input" />
+      <input
+        className="input-form__input"
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        name={name}
+        id={id}
+        onChange={onChange}
+        disabled={disabled}
+      />
 
-      <div className="input-form__right-icon" aria-hidden="true" />
+      {rightIcon && (
+        <div className="input-form__right-icon" aria-hidden="true">
+          {rightIcon}
+        </div>
+      )}
     </div>
   )
 }
